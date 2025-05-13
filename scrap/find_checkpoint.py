89f25f61eddb813dc53dde19db0bca23800af38e
RRomeroJr <117.rromero@gmail.com>
2025-05-13 11:21:11 -0400

    untracking and removing scrap

--- scrap/find_checkpoint.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from us_helpers import find_checkpoint
-
-path = find_checkpoint()["path"] + "/trainer_state.json"
-print(path)
-with open(path, "r", encoding="UTF-8") as f:
-    last_trainer_state = json.loads(f.read())
-print("final eval..", last_trainer_state["log_history"][-1])
